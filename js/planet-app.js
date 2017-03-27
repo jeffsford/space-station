9196221478187e2c31016bfe08c94ef19450d7c3
ADL175 <armydavidlim@gmail.com>
2017-03-27 15:05:30 -0700

    error on merge, trying to commit to fix

---
+++ js/planet-app.js
@@ -0,0 +1,98 @@
+'use strict';
+
+// Script to modify text pop-up balloons
+
+////////////////////////////////////////////////////////////////
+////////////////
+//////////////// Need to refactor the code below!!!
+////////////////
+////////////////////////////////////////////////////////////////
+
+// for(var i = 0; i < 10; i++){
+//   var popup[i] = document.getElementById('planet-div-[i]');
+//   popup[i].onmouseover = function(){
+//     document.getElementById('popupText[i]').style.display = 'block';
+//   };
+//   popup[i].onmouseout = function() {
+//     document.getElementById('popupText[i]').style.display = 'none';
+//   };
+// }
+
+////////////////////////////////////////////////////////////////
+////////////////
+//////////////// Need to refactor the code above!!!
+////////////////
+////////////////////////////////////////////////////////////////
+
+var popup1 = document.getElementById('planet-div-1');
+popup1.onmouseover = function(){
+  document.getElementById('popupText1').style.display = 'block';
+};
+popup1.onmouseout = function() {
+  document.getElementById('popupText1').style.display = 'none';
+};
+
+//end of popup script
+var popup2 = document.getElementById('planet-div-2');
+popup2.onmouseover = function(){
+  document.getElementById('popupText2').style.display = 'block';
+};
+popup2.onmouseout = function() {
+  document.getElementById('popupText2').style.display = 'none';
+};
+
+var popup3 = document.getElementById('planet-div-3');
+popup3.onmouseover = function(){
+  document.getElementById('popupText3').style.display = 'block';
+};
+popup3.onmouseout = function() {
+  document.getElementById('popupText3').style.display = 'none';
+};
+
+var popup4 = document.getElementById('planet-div-4');
+popup4.onmouseover = function(){
+  document.getElementById('popupText4').style.display = 'block';
+};
+popup4.onmouseout = function() {
+  document.getElementById('popupText4').style.display = 'none';
+};
+
+var popup5 = document.getElementById('planet-div-5');
+popup5.onmouseover = function(){
+  document.getElementById('popupText5').style.display = 'block';
+};
+popup5.onmouseout = function() {
+  document.getElementById('popupText5').style.display = 'none';
+};
+
+var popup6 = document.getElementById('planet-div-6');
+popup6.onmouseover = function(){
+  document.getElementById('popupText6').style.display = 'block';
+};
+popup6.onmouseout = function() {
+  document.getElementById('popupText6').style.display = 'none';
+};
+
+var popup7 = document.getElementById('planet-div-7');
+popup7.onmouseover = function(){
+  document.getElementById('popupText7').style.display = 'block';
+};
+popup7.onmouseout = function() {
+  document.getElementById('popupText7').style.display = 'none';
+};
+
+var popup8 = document.getElementById('planet-div-8');
+popup8.onmouseover = function(){
+  document.getElementById('popupText8').style.display = 'block';
+};
+popup8.onmouseout = function() {
+  document.getElementById('popupText8').style.display = 'none';
+};
+
+var popup9 = document.getElementById('planet-div-9');
+popup9.onmouseover = function(){
+  document.getElementById('popupText9').style.display = 'block';
+};
+popup9.onmouseout = function() {
+  document.getElementById('popupText9').style.display = 'none';
+};
