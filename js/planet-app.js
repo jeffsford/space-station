8eabcc37dc0f6ebe69e3089298478b66e0badcfb
ADL175 <armydavidlim@gmail.com>
2017-03-29 17:44:08 -0700

    fixed js error typo which was causing console errors

--- js/planet-app.js
+++ js/planet-app.js
@@ -6,7 +6,7 @@ function linkFunc() {
 window.onclick = function(event) {
   if (!event.target.matches('.linkbtn')) {
     var links = document.getElementsByClassName("link-content");
-    for (var i = 0; i < dropdowns.length; i++) {
+    for (var i = 0; i < links.length; i++) {
       var linkDropDown = links[i];
       if (linkDropDown.classList.contains('show')) {
         linkDropDown.classList.remove('show');
