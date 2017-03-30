6f4696dc5e4cc975faa97f5a438a6d04b861868d
micakenobi <beckyedithbrooker@gmail.com>
2017-03-30 09:53:44 -0700

    editing spacing on all pages

--- js/quiz.js
+++ js/quiz.js
@@ -7,7 +7,6 @@ function submitAnswers(e) {
   var total = 5;
   var score = 0;
 
-    //get user input
   var q1 = document.forms['qForm']['q1'].value;
   var q2 = document.forms['qForm']['q2'].value;
   var q3 = document.forms['qForm']['q3'].value;
@@ -15,7 +14,6 @@ function submitAnswers(e) {
   var q5 = document.forms['qForm']['q5'].value;
   var qArray = [q1, q2, q3, q4, q5];
 
-    //reminds user to select each button if left unselected
   for (var i = 0; i < qArray.length; i++) {
     if (qArray[i] === null || qArray[i] === '') {
       alert('You forgot to fill out question ' + [i + 1]);
@@ -23,16 +21,14 @@ function submitAnswers(e) {
     }
   }
 
-    //set correct answers
   var answers = ['a', 'b', 'b', 'c', 'a'];
 
-    //check answers
   for (var i = 0; i < qArray.length; i++) {
     if (qArray[i] === answers[i]) {
       score++;
     }
   }
-    //display results
+
   var results = document.getElementById('results');
   results.innerHTML = 'Nice job, you scored ' + score + ' out of ' + total + '.';
   return false;
